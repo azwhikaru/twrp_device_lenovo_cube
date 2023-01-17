61333b09556a9340547ca8d7f66e0d55102e7e49
azwhikaru <37921907+azwhikaru@users.noreply.github.com>
2023-01-17 14:14:33 +0800

    cube : Initial commit
    Initial commit

---
+++ device.mk
@@ -0,0 +1,42 @@
+LOCAL_PATH := device/lenovo/cube
+
+# A/B
+AB_OTA_PARTITIONS += \
+    boot \
+    system \
+    vendor
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service
+
+PRODUCT_PACKAGES += \
+    bootctrl.avb
+
+PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    bootctrl.avb \
+    libgptutils \
+    libz \
+    libcutils
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    $(LOCAL_PATH)/recovery/root/sbin/update_engine_sideload
+    
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery/root/init.recovery.mt8167.rc:root/init.recovery.mt8167.rc \
+    $(LOCAL_PATH)/recovery/root/init.recovery.service.rc:root/init.recovery.service.rc \
+    $(LOCAL_PATH)/recovery/root/ueventd.rc:root/ueventd.rc \
+    $(LOCAL_PATH)/recovery/root/ueventd.mt8167.rc:root/ueventd.mt8167.rc
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
---
+++ twrp_cube.mk
@@ -0,0 +1,19 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from cube device
+$(call inherit-product, device/lenovo/cube/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := cube
+PRODUCT_NAME := twrp_cube
+PRODUCT_BRAND := Lenovo
+PRODUCT_MODEL := cube
+PRODUCT_MANUFACTURER := Lenovo
+PRODUCT_RELEASE_NAME := Lenovo Smart Clock
